7f3c4bf430d19b8a1f5e5aed7904bed54f43a6fe
Wojtek Siudzinski <admin@suda.pl>
2015-09-04 16:20:53 +0100

    Export dependency resolver

--- main.js
+++ main.js
@@ -18,6 +18,7 @@
  * Created by middleca on 6/11/15.
  */
 
-var HalModuleParser = require('./lib/HalModuleParser.js');
-
-module.exports = HalModuleParser;
+module.exports = {
+  HalModuleParser: require('./lib/HalModuleParser.js'),
+  HalDependencyResolver: require('./lib/HalDependencyResolver.js')
+};
