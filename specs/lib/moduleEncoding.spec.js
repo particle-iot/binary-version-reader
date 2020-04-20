9ac8758c2ebffd1bd08c076786f06e24cb3c0912
Sergey Polyakov <sergeuz@gmail.com>
2020-04-20 20:11:35 +0200

    Add high-level API for compressing/decompressing module binaries

--- specs/lib/moduleEncoding.spec.js
+++ specs/lib/moduleEncoding.spec.js
@@ -2,8 +2,9 @@ const {
 	updateModuleSha256,
 	updateModuleCrc32,
 	updateModulePrefix,
-	updateModuleData,
-	extractModuleData
+	updateModuleSuffix,
+	compressModule,
+	decompressModule
 } = require('../../lib/moduleEncoding');
 
 const HalModuleParser = require('../../lib/HalModuleParser');
@@ -18,8 +19,17 @@ const path = require('path');
 const fs = require('fs');
 
 const TEST_BINARIES_PATH = path.resolve(path.join(__dirname, '../binaries'));
+
+// Exclude some old and malformed firmware binaries from the test
+const EXCLUDED_TEST_BINARIES = [
+	'emptybin.bin',
+	'RC2_bootloader.bin',
+	'RC4_bootloader_pad_BM-09.bin',
+	'RC4_bootloader_pad_BM-14.bin'
+];
+
 const TEST_BINARIES = fs.readdirSync(TEST_BINARIES_PATH)
-		.filter(f => f.endsWith('.bin'))
+		.filter(f => f.endsWith('.bin') && !EXCLUDED_TEST_BINARIES.includes(f))
 		.map(f => path.join(TEST_BINARIES_PATH, f));
 
 const SHA256_OFFSET = 38; // Relative to the end of the module data
@@ -98,12 +108,7 @@ describe('moduleEncoding', () => {
 		it('correctly updates test module binaries', async () => {
 			for (let file of TEST_BINARIES) {
 				const origBin = fs.readFileSync(file);
-				let prefixInfo = null;
-				try {
-					({ prefixInfo } = await parseModuleBinary(origBin));
-				} catch (err) {
-					continue; // Skip modules that can't be parsed
-				}
+				const { prefixInfo } = await parseModuleBinary(origBin);
 				const bin = Buffer.from(origBin);
 				bin.fill(0, prefixInfo.prefixOffset, prefixInfo.prefixOffset + MODULE_PREFIX_SIZE);
 				updateModulePrefix(bin, prefixInfo);
@@ -112,41 +117,60 @@ describe('moduleEncoding', () => {
 		});
 	});
 
-	describe('extractModuleData()', () => {
-		it('extracts the data from a module without a vector table', async () => {
-			const data = genRandomBuffer();
-			const bin = genModuleBinary({ data });
-			const { prefixInfo, suffixInfo } = await parseModuleBinary(bin);
-			const d = extractModuleData(bin, prefixInfo, suffixInfo);
-			expect(d.equals(data)).to.be.true;
+	describe('updateModuleSuffix()', () => {
+		it('can update suffix fields selectively', async () => {
+			const bin = genModuleBinary();
+			let suffixInfo = {
+				productId: 0x2211,
+				// productVersion: 0x4433,
+				reserved: 0x6655,
+				// fwUniqueId: '778899aabbccddeeff112233445566778899aabbccddeeff1122334455667788',
+				crcBlock: '99aabbcc',
+				suffixSize: 40 // Mandatory
+			};
+			const offs = bin.length - suffixInfo.suffixSize - 4;
+			const size = suffixInfo.suffixSize + 4; // Including CRC-32
+			bin.fill(0, offs, offs + size);
+			updateModuleSuffix(bin, suffixInfo);
+			let d = bin.slice(offs, offs + size);
+			expect(d.toString('hex')).to.equal('1122000055660000000000000000000000000000000000000000000000000000000000000000280099aabbcc');
+			suffixInfo = {
+				// productId: 0x2211,
+				productVersion: 0x4433,
+				// reserved: 0x6655,
+				fwUniqueId: '778899aabbccddeeff112233445566778899aabbccddeeff1122334455667788',
+				// crcBlock: '99aabbcc',
+				suffixSize: 40
+			};
+			bin.fill(0, offs, offs + size);
+			updateModuleSuffix(bin, suffixInfo);
+			d = bin.slice(offs, offs + size);
+			expect(d.toString('hex')).to.equal('000033440000778899aabbccddeeff112233445566778899aabbccddeeff1122334455667788280000000000');
 		});
 
-		it('extracts the data from a module with a vector table', async () => {
-			const data = genRandomBuffer();
-			const bin = genModuleBinary({ data, addVectorTable: true });
-			const { prefixInfo, suffixInfo } = await parseModuleBinary(bin);
-			const d = extractModuleData(bin, prefixInfo, suffixInfo);
-			expect(d.equals(data)).to.be.true;
+		it('correctly updates test module binaries', async () => {
+			for (let file of TEST_BINARIES) {
+				const origBin = fs.readFileSync(file);
+				const { suffixInfo } = await parseModuleBinary(origBin);
+				const bin = Buffer.from(origBin);
+				const offs = bin.length - suffixInfo.suffixSize - 4;
+				const size = suffixInfo.suffixSize + 4; // Including CRC-32
+				bin.fill(0, offs, offs + size);
+				updateModuleSuffix(bin, suffixInfo);
+				expect(bin.equals(origBin)).to.be.true;
+			}
 		});
 	});
 
-	describe('updateModuleData()', () => {
-		it('updates the data of a module without a vector table', async () => {
-			let bin = genModuleBinary();
-			const { prefixInfo, suffixInfo } = await parseModuleBinary(bin);
-			const data = genRandomBuffer();
-			bin = updateModuleData(bin, data, prefixInfo, suffixInfo);
-			const d = extractModuleData(bin, prefixInfo, suffixInfo);
-			expect(d.equals(data)).to.be.true;
-		});
-
-		it('updates the data of a module with a vector table', async () => {
-			let bin = genModuleBinary({ addVectorTable: true });
-			const { prefixInfo, suffixInfo } = await parseModuleBinary(bin);
-			const data = genRandomBuffer();
-			bin = updateModuleData(bin, data, prefixInfo, suffixInfo);
-			const d = extractModuleData(bin, prefixInfo, suffixInfo);
-			expect(d.equals(data)).to.be.true;
+	describe('compressModule() / decompressModule()', () => {
+		it('compress and decompress test module binaries', async () => {
+			for (let file of TEST_BINARIES) {
+				const origBin = fs.readFileSync(file);
+				const compBin = await compressModule(origBin);
+				expect(compBin.length).to.be.lessThan(origBin.length);
+				const decompBin = await decompressModule(compBin);
+				expect(decompBin.equals(origBin)).to.be.true;
+			}
 		});
 	});
 
